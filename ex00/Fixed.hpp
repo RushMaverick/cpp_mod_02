7d01b9c567c5aa359d75abea617e4c62257c02d0
Rasmus Rask <rasse.rask@gmail.com>
2023-10-27 11:27:17 +0300

    wrote Fixed class

--- ex00/Fixed.hpp
+++ ex00/Fixed.hpp
@@ -1,12 +1,19 @@
 #ifndef FIXEDHPP
 #define FIXEDHPP
 
+#include <iostream>
+
 class Fixed {
 	private:
 		int _value;
 		static const int _fract = 8;
 	public:
-		
+		Fixed();
+		~Fixed();
+		Fixed(const Fixed &other);
+		Fixed &operator=(const Fixed &other);
+		int getRawBits(int const raw);
+		int setRawBits(int const raw);
 };
 
 #endif
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,3 +1,12 @@
-int main (void) {
-	return (0);
+#include "Fixed.hpp"
+
+int main( void ) {
+	Fixed a;
+	Fixed b( a );
+	Fixed c;
+	c = b;
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+	return 0;
 }
